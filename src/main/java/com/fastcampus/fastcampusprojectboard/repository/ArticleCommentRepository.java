1c5f04d2882e096dcaeed6d51b5d451e5f84e76f
backwizard0 <gkaeogus7@gmail.com>
2023-02-13 14:56:29 +0900

    .

--- src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
@@ -7,13 +7,16 @@ import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
         JpaRepository<ArticleComment, Long>,
-        QuerydslPredicateExecutor<QArticleComment>
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
 {
     @Override
     default void customize(QuerydslBindings bindings, QArticleComment root){
