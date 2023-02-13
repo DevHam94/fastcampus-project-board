0a74ba426a7794b756c4ad5da7a306ba102d88e0
backwizard0 <gkaeogus7@gmail.com>
2023-02-13 14:16:35 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,26 @@
+package com.fastcampus.fastcampusprojectboard.repository;
+
+import com.fastcampus.fastcampusprojectboard.domain.ArticleComment;
+import com.fastcampus.fastcampusprojectboard.domain.QArticle;
+import com.fastcampus.fastcampusprojectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+
+
+@RepositoryRestResource
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+}
